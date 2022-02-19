97b8803a201c1213ef691eb3704239560c2d4ac0
Mukhametrakhimov Rustam <ruskb16@gmail.com>
2022-02-19 23:26:09 +0500

    complite task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const brackets = bracketsConfig.map(el => el.join(''));
+  let temp = str;
+  let len = brackets.length;
+  while (temp.length > 0) {
+    let tmp2 = temp;
+    for (let i = 0; i < len; i++) {
+      if (temp.includes(brackets[i])) {
+        temp = temp.replace(brackets[i], '');
+      };      
+    };
+    if (tmp2 == temp) return false;
+  };  
+  return true;
+};
